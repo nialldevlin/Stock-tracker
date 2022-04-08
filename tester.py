344e18eacf62c9d6f49d7ac36e7599fe1ed3b198
nialldevlin <32718927+nialldevlin@users.noreply.github.com>
2022-04-08 13:23:08 -0600

    Testing new strategy

--- tester.py
+++ tester.py
@@ -1,11 +1,32 @@
 #!../stracker/bin/python3
-from stock_analyzer import Stockalyzer
-stock = 'VTI'
-s = Stockalyzer(stock, interval='60min')
-#print(s.analysis)
-#s.saveAsPng()
-s.display()
-"""print(s.tiaanalysis(mode='store'))
-balance, num_stock = s.runSimulationTI(1000, 1, verbose=True)
-total = balance + num_stock * tick.info['currentPrice']
-print("{} 100d Simulation: Final balance: {}, Final stock {}, Total: {}".format(stock, balance, num_stock, total))"""
+
+from tradingview_ta import TA_Handler, Interval, Exchange
+import tradingview_ta
+
+ticker = 'WMT'
+
+handler = TA_Handler(
+    symbol=ticker,
+    exchange="NYSE",
+    screener="america",
+    interval=Interval.INTERVAL_1_DAY
+)
+
+analysis = handler.get_analysis()
+
+oscillators = analysis.oscillators
+indicators = analysis.indicators
+summary = analysis.summary
+
+rsi = indicators['RSI']
+stochk = indicators['Stoch.K']
+stochd = indicators['Stoch.D']
+macd = indicators['MACD.macd']
+sig = indicators['MACD.signal']
+
+if rsi > 50 and stochk > 50 and stochd > 50 and macd > sig:
+	powerXAnalysis = 'Buy'
+elif rsi < 50 and stochk < 50 and stochd < 50 and macd < sig:
+	powerXAnalysis = 'Sell'
+else:
+	powerXAnalysis = 'Hold'
