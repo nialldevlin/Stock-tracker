ad802bdcf7b3448439393f8f2812c3295e74e425
Niall Devlin <niallcdevlin@gmail.com>
2022-05-20 22:45:01 -0600

    Added gitignore

--- run_screener.py
+++ run_screener.py
@@ -6,8 +6,9 @@ import sqlite3
 import os
 
 if __name__ == '__main__':
-	screener = Screener()
-	data = screener.data
+	#screener = Screener()
+	#data = screener.data
+	data = pd.DataFrame({'Test': [1, 2, 3], 'Test 1': [4, 5, 6], 'Test 2': [7, 8, 9]})
 	
 	conn = sqlite3.connect("stockdb.sqlite")
 	c = conn.cursor()
@@ -15,12 +16,13 @@ if __name__ == '__main__':
 	sql = "DROP TABLE IF EXISTS stockdb"
 	c.execute(sql)
 
-	sql = "CREATE TABLE IF EXISTS stockdb ("
+	sql = "CREATE TABLE stockdb ("
 	for col in data.columns:
 		sql += col
 		sql += ", "
 	sql = sql[:-1]
+	sql += ')'
 	c.execute(sql)
-
-	df.to_sql('stockdb', con=conn, if_exists='replace')
+	
+	data.to_sql('stockdb', con=conn, if_exists='replace')
 	
