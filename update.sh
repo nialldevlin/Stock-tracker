f5bd8ae9fa3087dfa77ca07c10af07d6090dcbaa
Niall Devlin <niallcdevlin@gmail.com>
2022-05-25 14:33:33 -0600

    Updated database, refactored

--- update.sh
+++ update.sh
@@ -2,7 +2,6 @@
 git add .
 git commit -m $1
 git push
-sudo rm -r /var/www/html/*
 sudo cp -r /home/proffessordevnito/Documents/Python_Projects/Stock-tracker/* /var/www/html/
 sudo rm /var/www/html/update.sh
 
