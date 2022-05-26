cc4c453c7b2d5124d611b36c811049716fecc56f
Niall Devlin <niallcdevlin@gmail.com>
2022-05-26 11:50:58 -0600

    Cleaning up

--- gitignoreupdate.sh
+++ gitignoreupdate.sh
@@ -3,3 +3,4 @@ echo $1 >> .gitignore
 git rm -r --cached .
 git add -A
 git commit -am 'Removing ignored files'
+git push
--- stock_data.js
+++
@@ -1,4 +0,0 @@
-var analysis = document.getElementById("stock-analysis").value;
-var symbol = document.getElementById("Symbol").value;
-var min_price = document.getElementById("min-price").value;
-var max_price = document.getElementById("max-price").value;
